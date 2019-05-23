e7edad634da9df2433e790f84b3ffa600527465e
acheval <45897888+acheval@users.noreply.github.com>
2019-05-23 09:32:26 +0200

    First commit
    Powershell script to change ip, mask, dns, gateway, name and domain integrate on a machine

---
+++ ip_and_domain_change.ps1
@@ -0,0 +1,65 @@
+# Logging
+$DesktopPath= [Environment]::GetFolderPath("Desktop")
+$Stamp = (Get-Date).toString("yyyyMMdd-HHmmss")
+Start-Transcript -Append -Path $DesktopPath\$Stamp"_script.log"
+
+#Input the details
+    #Network details
+[ipaddress]$IPAddress= Read-Host -Prompt "Enter the new IPv4 address of the machine"
+    $IPAddress = $IPAddress.IPAddressToString
+
+[ValidateRange(1,32)]$MaskBits = Read-Host -Prompt "Enter the mask bits"
+
+[ipaddress]$GatewayIPAddress = Read-Host -Prompt "Please enter the defaut gateway's IPv4 Address" 
+    $GatewayIPAddress = $GatewayIPAddress.IPAddressToString
+
+[ipaddress]$DNSIPAddress = Read-Host -Prompt "Please enter the DNS IPv4 Address"
+    $DNSIPAddress = $DNSIPAddress.IPAddressToString
+
+$IPType = "IPv4"
+
+    #Domain details
+
+[string]$LocalCredential= Read-Host -Prompt "Input a local admin username"
+
+[string]$DomainCredential= Read-Host -Prompt "Input a domain user capable of integrating new computers"
+
+[string]$NewDomain = Read-Host -Prompt "Input the machine's new domain"
+
+[string]$OUPath= Read-Host -Prompt "Input the Distinguished Name OU Path"
+
+# Select the adapter
+Get-NetAdapter | ? {$_.Status -eq "up"}
+$NetworkInterfaceID= Read-Host -Prompt "Enter the interface index of the NIC you want to modify"
+
+# Review the input
+Write-Host "IP Address:" $IPAddress"/"$MaskBits
+Write-Host "Gateway:" $GatewayIPAddress
+Write-Host "DNS Server:" $DNSIPAddress
+Write-Host "To be set on Interface :" $NetworkInterfaceID
+Write-Host "Computer will be renamed with local user:" $LocalCredential
+Write-Host "To be integrated in:" $NewDomain
+Write-Host "With domain user:" $DomainCredential
+Write-Host "Under the" $OUPath "OU"
+
+[void](Read-Host "Press Enter to continue")
+
+# Remove any existing IP, gateway from our ipv4 adapter
+If ((Get-NetIPConfiguration -InterfaceIndex $NetworkInterfaceID).IPv4Address.IPAddress) {
+    Remove-NetIPAddress -InterfaceIndex $NetworkInterfaceID -AddressFamily $IPType -Confirm:$false
+}
+
+If ((Get-NetIPConfiguration -InterfaceIndex $NetworkInterfaceID).Ipv4DefaultGateway) {
+    Remove-NetRoute -InterfaceIndex $NetworkInterfaceID -AddressFamily $IPType -Confirm:$false
+}
+
+New-NetIPAddress –InterfaceIndex $NetworkInterfaceID -AddressFamily $IPType –IPAddress $IPAddress –PrefixLength $MaskBits -DefaultGateway $GatewayIPAddress
+
+Set-DnsClientServerAddress -InterfaceIndex $NetworkInterfaceID -ServerAddresses $DNSIPAddress
+
+Start-Sleep -s 10
+
+$FQDNNameHost= Resolve-DnsName $IPAddress | Select-Object -ExpandProperty NameHost
+    $DNSNameHost= $FQDNNameHost.split(".")[0]
+    
+Add-Computer -LocalCredential $LocalCredential@$env:ComputerName -NewName $DNSNameHost -Credential $DomainCredential@$NewDomain -DomainName $NewDomain -Options AccountCreate -OUPath $OUPath -Confirm -Restart
